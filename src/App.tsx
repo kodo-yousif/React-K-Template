ea3d480a7e1833f9fb4af5d5e28cfb9faea1ae5c
Kodo Yousif <kodo.yousif@gmail.com>
2024-03-03 22:47:16 +0300

    svgr and absolute paths added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import Header from "@/components/Header"
+import Kilwa from "@/assets/71QlZuD0h2L._AC_SX466_.jpg"
+
 export default function App() {
   return (
     <div className="bg-red-500">
+      <Header />
+      <div>
+        <img src={Kilwa} />
+      </div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   )
