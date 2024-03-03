1882f757883b3eae28d8d7a7c9bc3688c62293eb
Kodo Yousif <kodo.yousif@gmail.com>
2024-03-03 22:06:45 +0300

    +

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
 export default function App() {
-  return <div className="bg-red-500">App</div>
+  return (
+    <div className="bg-red-500">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  )
 }
